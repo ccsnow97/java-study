30e81e9615fab2d326a1f7259d8eb35cb1541a8f
congcong <ccsnow97@163.com>
2020-06-29 10:41:13 +0800

    listNode

--- jdk/src/main/java/com/cc/study/jdk/Test.java
+++ jdk/src/main/java/com/cc/study/jdk/Test.java
@@ -9,42 +9,49 @@ public class Test {
 
     public static void main(String[] args) {
         Solution solution = new Test().new Solution();
-        Integer[] array = {3,9,20,null,null,15,7};
+        Integer[] array = {3, 9, 20, null, null, 15, 7};
         TreeNode treeNode = TreeNode.flatToTree(new ArrayList<>(Arrays.asList(array)));
-        boolean palindrome = solution.isPalindrome("A man, a plan, a canal: Panama");
+        int[] test = {5, 7, 7, 8, 8, 10};
+        char s = ' ';
+        String s1 = Integer.toBinaryString(s);
+        long start = System.currentTimeMillis();
+        int i = solution.superEggDrop(3, 14);
+        double time = (System.currentTimeMillis() - start) / 1000.0;
     }
 
     class Solution {
-        public boolean isPalindrome(String s) {
-            if(s==null||s.length()==0) return true;
-            int length = s.length();
-            int start = 0;
-            int end = length-1;
-            while(end>start){
-                if(!isValid(s.charAt(start))) {start++; continue;}
-                if(!isValid(s.charAt(end))) {end--;continue;}
-                char startCh = s.charAt(start);
-                char endCh = s.charAt(end);
-                if(startCh>='a') startCh-=32;
-                if(endCh>='a') endCh-=32;
-                if(startCh==endCh) {
-                    start++;end--;
-                }else {
-                    return false;
+        public int superEggDrop(int K, int N) {
+            int[][] p = new int[K + 1][N + 1];
+            for (int n = 1; n <= N; n++) p[1][n] = n;
+            for (int k = 2; k <= K; k++) {
+                for (int n = 1; n <= N; n++) {
+                    int res = Integer.MAX_VALUE;
+                    int start = 1, end = n, mid;
+                    /*  这是遍历
+                    for (int i = 1; i <= n; i++) {
+                        res = Math.min(res, Math.max(p[k - 1][i - 1], p[k][n - i]) + 1);
+                    }
+                    */
+                    //这是二分
+                    while (start <= end) {
+                        mid = start + (end - start) / 2;
+                        if (p[k - 1][mid - 1] == p[k][n - mid]) {
+                            res = p[k - 1][mid - 1] + 1;
+                            break;
+                        } else if (p[k - 1][mid - 1] > p[k][n - mid]) {
+                            end = mid - 1;
+                            res = Math.min(res, p[k - 1][mid - 1] + 1);
+                        } else if (p[k - 1][mid - 1] < p[k][n - mid]) {
+                            start = mid + 1;
+                            res = Math.min(res, p[k][n - mid] + 1);
+                        }
+                    }
+                    p[k][n] = res;
                 }
             }
-            return true;
+            return p[K][N];
         }
 
-        public boolean isValid(Character ch){
-            if(ch>='0'&&ch<='9'
-                    ||ch>='a'&&ch<='z'
-                    ||ch>='A'&&ch<='Z'){
-                return true;
-            }
-            return false;
-        }
     }
 
 }
-
